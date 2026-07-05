28f426853dc6971a37f9a414c393c481bc294a39
Ramon Alejandro Aguirre <tatitagood2004@gmail.com>
2026-07-05 11:47:54 -0300

    Arregle un par de cosas sobre mis comentarios y modifique varios elementos

--- script.js
+++ script.js
@@ -3,14 +3,13 @@
 const usuariosArray = ['Ana', 'Luis', 'María'];
 console.log(usuariosArray[1]); // ¿Qué nombre aparece aquí?
 
-// El nombre que aparece en la consola es "Luis", porque esta en el indice 1 de el attay de usuarios
-
+// El nombre que aparecé en la consola es "Luis", porque está en el índice 1 del array de usuarios
+ 
 // Ejercicio Numero 4
+
 // Agregar un nuevo usuario
 
-let nuevoUsuario = 'Carlos';
-usuariosArray.push(nuevoUsuario);
-console.log(nuevoUsuario); 
+usuariosArray.push('Carlos');
 console.log(usuariosArray); 
 
 // Eliminar un usuario
@@ -25,20 +24,20 @@ const usuariosObjeto = {
   usuario2: 'Luis',
   usuario3: 'María'
 };
+
 console.log(usuariosObjeto.usuario2); // ¿Qué nombre aparece aquí?
 
-// El nombre que aparece en la consola es nuevamente "Luis", porque hacemos el llamado a la propiedad de "usuario2" del objeto usuarioObjetos.
+// El nombre que aparece en la consola es nuevamente "Luis",  hacemos el llamado a la propiedad "usuario2" del objeto.
 
 
 // Ejercicio Numero 3
 
 //¿Cuál estructura es más fácil de recorrer si quiero mostrar todos los usuarios?
 
-// la manera mas facil de recorrer a todo los usuarios es con el array, ya que podemos usar elementos como for o forEach para recorrerlo.
-
+// La manera más fácil de recorrer todos los usuarios es mediante un array, ya que podemos utilizar un for o un forEach para recorrer sus elementos.
 // ¿Cuál es más adecuada para una lista que puede crecer o cambiar de tamaño?
 
 // La estructura más adecuada para una lista que puede crecer o cambiar de tamaño es un array, ya que permite agregar o eliminar elementos de manera sencilla.
 
 // Paso 5: ¿Crees que sería igual de sencillo hacer estas operaciones con el objeto? ¿Por qué?
-//no, no seria igual de sencillo que hacer estas operaciones con el objeto, ya que los objetos no tienen metodos asi como los array mas sencillo de agregar o eliminar elementos.
+// No sería igual de sencillo, porque los objetos no tienen métodos como push() o shift(). Para agregar una propiedad debemos asignarla manualmente y para eliminarla usamos delete.
